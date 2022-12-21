1065623d7d6183166c16ab0f1587348a5aa9eee6
Maria de Fátima <maria.defatimabarbosacardoso@gmail.com>
2022-12-21 20:29:02 -0300

    ensure LoginRouter returns 400 if an invalid email is provided

--- src/presentation/helpers/missing-param-error.js
+++ src/presentation/helpers/missing-param-error.js
@@ -1,6 +1,6 @@
 module.exports = class MissingParamError extends Error {
   constructor (paramName) {
-    super(`Missing para: ${paramName}`)
+    super(`Missing param: ${paramName}`)
     this.name = 'MissingParamError'
   }
 }
